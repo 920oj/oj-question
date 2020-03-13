9f134bc5be81e1345a7ac7d4a9e80c5c55a3d18d
Junya Oiwa <0920oiwa@gmail.com>
2020-03-13 21:38:31 +0000

    [add]ページネーション実装

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -9,6 +9,15 @@
         <h3>これまでの質問</h3>
         <QuestionList :list="display_question_list[current - 1]" />
       </div>
+      
+      <div class="pagination">
+        <div class="pagination-left-btn" v-if="pagination_style == 'common' || pagination_style == 'left_only'" @click="movePage(-1)">
+          <Btn title="前へ" />
+        </div>
+        <div class="pagination-right-btn" v-if="pagination_style == 'common' || pagination_style == 'right_only'" @click="movePage(1)">
+          <Btn title="次へ" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,6 +26,7 @@
 import Header from '~/components/layouts/header.vue'
 import QuestionForm from '~/components/layouts/questionForm.vue'
 import QuestionList from '~/components/layouts/questionList.vue';
+import Btn from '~/components/ui/btn.vue'
 
 export default {
   data() {
@@ -24,6 +34,7 @@ export default {
       current: 1,
       max_row: 1,
       display_question_list: [],
+      pagination_style: 'common'
     }
   },
 
@@ -31,6 +42,7 @@ export default {
     Header,
     QuestionForm,
     QuestionList,
+    Btn,
   },
 
   mounted() {
@@ -43,14 +55,33 @@ export default {
 
     this.max_row = max
     this.display_question_list = d_list
+    this.judgePaginationStyle();
   },
 
-  computed: {
-
+  watch: {
+    current: function() {
+      this.judgePaginationStyle()
+    }
   },
 
   methods: {
-    
+    judgePaginationStyle: function() {
+      if(this.max_row == 1) {
+        this.pagination_style = 'none'
+      }
+      else if(this.current == 1) {
+        this.pagination_style = 'right_only'
+      }
+      else if(this.current == this.max_row) {
+        this.pagination_style = 'left_only'
+      }
+      else {
+        this.pagination_style = 'common'
+      }
+    },
+    movePage: function(value) {
+      this.current += value
+    }
   },
 
   async asyncData({ $axios }) {
@@ -75,5 +106,11 @@ export default {
   text-align: center;
   margin: 50px 0;
   margin-bottom: 30px;
+}
+
+.pagination {
+  width: 100%;
+  display: flex;
+  justify-content: center;
 }
 </style>

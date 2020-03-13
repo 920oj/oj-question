5d8ad9a0d08d66be9c808e94b5fb8f8d573b9053
Junya Oiwa <0920oiwa@gmail.com>
2020-03-13 11:36:39 +0000

    [add]フロント・ヘッダーコンポーネントを作成

---
+++ client/components/layouts/header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    <div class="header-wrapper">
+      <p>OJの質問BOX</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.header {
+  height: 60px;
+}
+
+.header-wrapper {
+  height: 60px;
+  background: linear-gradient(90deg, rgba(34,166,224,1) 0%, rgba(5,213,255,1) 100%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.header-wrapper p{
+  font-weight: 900;
+  font-size: 1.3rem;
+  color: #FFF;
+}
+
+</style>
